e908305cd8a0e8c895b238681c28be99dbf7bf76
S_Ariel <xaxwtf@gmail.com>
2022-03-17 02:12:52 -0300

    termine mercado liebre

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
-app.listen(process.env.PORT||3000, () => {
+app.listen(process.env.PORT|| 3000, () => {
     console.log('Servidor Funcionando');
 
 });
@@ -11,5 +11,10 @@ console.log(__dirname);
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/home.html');
-
 });
+app.get('/register',(req, res)=>{
+    res.sendFile(__dirname + '/views/register.html' );
+});
+app.get('/login',(req,res)=>{
+    res.sendFile(__dirname +'/views/login.html');
+})
